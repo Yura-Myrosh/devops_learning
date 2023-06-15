bf9f8cd5220793502a64ddf7f6178422086c70c8
Yura-Myrosh <72310926+Yura-Myrosh@users.noreply.github.com>
2023-06-15 12:11:38 +0300

    Initial shopping api added

---
+++ Shopping/Shopping.API/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Shopping.API.Models
+{
+    public class Product
+    {
+        public string Id { get; internal set; }
+        public string Name { get; internal set; }
+        public string Category { get; internal set; }
+        public string Description { get; internal set; }
+        public string ImageFile { get; internal set; }
+        public decimal Price { get; internal set; }
+    }
+}
